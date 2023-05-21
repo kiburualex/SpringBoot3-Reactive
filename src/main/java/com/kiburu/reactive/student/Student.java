6561db87b29e47fc62d6b103bfe14ee9f3b36d8b
Alex Kiburu <kiburu.alex@eclectics.io>
2023-05-21 09:51:44 +0300

    - Initial commit - Add flyway plugin for migrations - Add migrations directory with sql script - Configure property file to handle reactive db connection and flyway jdbc connection

---
+++ src/main/java/com/kiburu/reactive/student/Student.java
@@ -0,0 +1,23 @@
+package com.kiburu.reactive.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "students")
+public class Student {
+
+    @Id
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private int age;
+
+}
